885ae4fa5babe314f283629dcdb98c10242dfe02
Om <uikeyom1234@gmail.com>
2024-02-20 21:24:55 +0530

    Static Css manage and work on sub nav of dealer dashboard with using toggle so aaded some javascript files and jquery

---
+++ Booking/Car/static/javascript/jquery/toggle.js
@@ -0,0 +1,33 @@
+// Dealer dashboard menu first product sub section
+$(document).ready(function () {
+    // Toggle the element when the button is clicked
+    $("#menu-product").click(function () {
+        $("#menu-product-data").slideToggle();
+    });
+});
+
+
+// Dealer dashboard menu user sub section
+$(document).ready(function () {
+    // Toggle the element when the button is clicked
+    $("#menu-user").click(function () {
+        $("#menu-user-data").slideToggle();
+    });
+});
+
+
+// Dealer dashboard menu payment sub section
+$(document).ready(function () {
+    // Toggle the element when the button is clicked
+    $("#menu-payment").click(function () {
+        $("#menu-payment-data").slideToggle();
+    });
+});
+
+
+// Dealer dashboard menu account sub section
+$(document).ready(function () {
+    $("#menu-account").click(function () {
+        $("#menu-account-data").slideToggle();
+    });
+});
